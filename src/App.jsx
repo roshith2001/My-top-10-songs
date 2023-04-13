45bf79ec7e4c8cf78759081f7bf5d4ddef79399a
roshith2001 <roshithj@gmail.com>
2023-04-13 19:02:39 +0530

    Netlify Debug - Invalid redirect URL cleared

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [isAuthorized, setIsAuthorized] = useState(true);
 
   const CLIENT_ID = '5f003b83688d434a884e22441ae85f4f';
-  const REDIRECT_URI = 'http://https://hearmy10.netlify.app';
+  const REDIRECT_URI = 'https://hearmy10.netlify.app';
   const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
   const RESPONSE_TYPE = 'token';
 
