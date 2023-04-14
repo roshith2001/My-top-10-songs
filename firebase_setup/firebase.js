c26f5bf190c2482079904d4ca665e0f97449df2b
roshith2001 <roshithj@gmail.com>
2023-04-14 11:19:23 +0530

    Still dealing with Firebase

--- firebase_setup/firebase.js
+++ firebase_setup/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBGfErv8dUwsU7HUs_K7PArIc1TyrlZHFg",
-  authDomain: "hear-my-10-4d020.firebaseapp.com",
-  projectId: "hear-my-10-4d020",
-  storageBucket: "hear-my-10-4d020.appspot.com",
-  messagingSenderId: "354808717136",
-  appId: "1:354808717136:web:0158a94827af3dbc3f9655"
+  apiKey: "AIzaSyDmtpzO6jxmF52zYw6xGWVkzb6dwCR5knY",
+  authDomain: "hear-my-10-73ea9.firebaseapp.com",
+  projectId: "hear-my-10-73ea9",
+  storageBucket: "hear-my-10-73ea9.appspot.com",
+  messagingSenderId: "145644027884",
+  appId: "1:145644027884:web:b5f05dfb6d9839d688cac9"
 };
 
 // Initialize Firebase
